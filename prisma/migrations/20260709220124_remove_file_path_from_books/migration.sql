11b5ce90b92ae56ff7809dd8a3bbcce6081a732e
Sofia <sachettisofia@gmail.com>
2026-07-09 19:03:34 -0300

    fix: elimino filePath de la estructura books

---
+++ prisma/migrations/20260709220124_remove_file_path_from_books/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `file_path` on the `books` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `books` DROP COLUMN `file_path`;
